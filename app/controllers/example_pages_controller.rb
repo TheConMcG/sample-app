4b9eefde3228dc8a88611cf89fbb2656feeaf5ac
Connor <conmmcguire@gmail.com>
2021-10-10 10:27:35 -0500

    create 3 different paths

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,10 @@ class ExamplePagesController < ApplicationController
   def hello_action
     render json: {message: "hello"}
   end
+  def goodbye_action
+    render json: {message: "goodbye"}
+  end
+  def hear_action
+    render json: {message: "Can you hear me?"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello_path", controller: "example_pages", action: "hello_action"
+  get "/goodbye_path", controller: "example_pages", action: "goodbye_action"
+  get "/hear_path", controller: "example_pages", action: "hear_action"
 end
